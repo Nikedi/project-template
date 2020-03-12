c6eb3fe0a1faa37dc997d812940b4f55671b048c
Niko Kataja <niko.kataja@yahoo.com>
2020-03-12 08:38:21 +0200

    added charts

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {Table} from './components/tempTable.jsx'
+import {Line} from 'react-chartjs-2';
 
 import './assets/stylesheets/style.css';
 
@@ -12,6 +13,41 @@ const baseUrl = window.location.hostname;
 const backendUrl = `http://${baseUrl}:${BACKEND_PORT}`;
 
 /* ADD YOUR CODE AFTER THIS LINE */
+
+const getData = (events) => ({
+
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+	data: events.slice(events.length - 100).map(event => {
+	    const timestamp = event.timestamp
+	    const temperature = event.temperature
+	    console.log(temperature)
+	    return {
+		x: timestamp,
+		y: temperature
+	    }
+	})
+    }
+  ]
+})
 
 const getGreetingFromBackend = async () => {
   try {
@@ -73,11 +109,20 @@ class App extends Component {
       console.log(events)
       this.setState({ greeting: response.greeting,events: events.results });
   }
-
-  render() {
+    render() {
       return (
 	  <>
-	<Table events={this.state.events}/>
+	    <Line
+	      data={getData(this.state.events)}
+	      options={{
+		  scales: {
+		      xAxes:[{
+			  type: 'time',
+			  distribution: 'series',  
+		      }]
+		  }
+	      }}
+	      />
 	  </>
       );
   }
