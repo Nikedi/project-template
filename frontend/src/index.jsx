a3a6995281fbabc931ad1c71b2e9813f04052226
Niko Kataja <niko.kataja@yahoo.com>
2020-03-18 16:16:23 +0200

    Added slice of 60

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -57,7 +57,7 @@ const getData = (events) => ({
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-	data: getMinuteAverage(events.sort(function (a,b) {return new Date(a.x) - new Date(b.x)})).map(event => {
+	data: getMinuteAverage(events.sort(function(a,b) {return new Date(a.x) - new Date(b.x)})).slice(-60).map(event => {
 	    console.log(event)
 	    const timestamp = event.timestamp
 	    const temperature = event.temperature
